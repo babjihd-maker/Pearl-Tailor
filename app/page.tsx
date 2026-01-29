77574c51e7eb21ec429ad2053b7410db59d2eac1
babjihd-maker <babjihd@gmail.com>
2026-01-29 21:18:23 +0530

    Fix mobile responsive and redirect to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  // Instead of showing the "System Check" text, 
-  // this instantly sends the user to your beautiful Login page
+  // This sends you instantly to the real Login Page
   redirect('/login');
 }
